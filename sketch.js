bad3b0f57d80e8bd6afee4d8479a91d42d898fbf
Esme <aesme6@gmail.com>
2020-01-25 00:08:39 -0500

    replacing template code with test code from p5 editor

--- sketch.js
+++ sketch.js
@@ -1,14 +1,25 @@
 function setup() {
-	createCanvas(800,600); // make an HTML canvas element width x height pixels
+  createCanvas(600, 600);
 }
 
 function draw() {
-	background(225);
-	textSize(32);
-	fill(180);
-	text(hour(), 10, 30);
-	fill(100);
-	text(minute(), 10, 60);
-	fill(0);
-	text(second(), 10, 90);
+  
+  background(255);
+  noStroke();
+  
+  // hour
+  fill(255, 204, 0);
+  let h = hour();
+  map(circle(h, h, 100), 0, 100, 0, width);
+  
+  // minute 
+  fill('rgba(0,255,0, 0.25)');
+  let m = minute();
+  map(circle(m, m, 50), 0, 100, 0, width);
+  
+  // second 
+  fill('rgba(100%,0%,100%,0.5)');
+  let s = second();
+  map(circle(s, s, 15), 0, 100, 0, width); 
+  
 }
